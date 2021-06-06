88222cfde265a3bcfec9d7a753d26c1eb8b2b7fa
Sanjaykolpady <71876219+Sanjaykolpady@users.noreply.github.com>
2021-06-06 12:45:33 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="Note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This is a great bootcamp. Anyone can learn Javascript and React.js from
+        scratch.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ index.js
@@ -0,0 +1,10 @@
+import react from "react";
+import reactdom from "react-dom";
+import App from "../src/components/App";
+
+reactdom.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
